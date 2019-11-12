8bd1e791f33e6d1f198ee06c239566b1ed247ab0
beatz4 <beatz148@gmail.com>
2019-11-12 22:30:45 +0900

    'chapter9ing'

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//import { throws } from "assert";
+
 //import { WriteStream } from "fs"
 
 // $(document).ready(function() {
@@ -434,10 +436,38 @@
 // console.log(cards.filter(c => c.value === 2).map(cardToString));
 // console.log(cards.filter(c => c.value > 10 && c.suit === 'H').map(cardToString));
 
-const SYM = Symbol();
-const o = {a: 1, b: 2, c: 3, [SYM]: 4};
+// const SYM = Symbol();
+// const o = {a: 1, b: 2, c: 3, [SYM]: 4};
 
-for (let prop in o) {
-    if (!o.hasOwnProperty(prop)) continue;
-    console.log(`${prop}: ${o[prop]}`);
-}
+// for (let prop in o) {
+//     if (!o.hasOwnProperty(prop)) continue;
+//     console.log(`${prop}: ${o[prop]}`);
+// }
+
+
+const Car = function() {
+
+    const carProps = new WeakMap();
+    class Car {
+        constructor(make, model) {
+            this.make = make;
+            this.model = model;
+            this._userGears = ['P', 'N', 'R', 'D'];
+            carProps.set(this, { userGear: this._userGears[0]});
+        }
+        get userGear() { return carProps.get(this).userGear; }
+        set userGear(value) {
+            if(this._userGears.indexOf(value) < 0)
+                throw new Error(`Invalid gear:${value}`);
+            carProps.get(this).userGear = value;
+        }
+        shift(gear) { this.userGear = gear; }
+    }
+
+    return Car;
+}();
+
+const car1 = new Car("Tesla", "Model S");
+const car2 = new Car("Mazda", "3i");
+car1.shift('D');
+car2.shift('R');
