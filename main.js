a6fa06b0d5a3220ed07cb92af2aec00e529f3ec4
jhnoh <beatz4@naver.com>
2019-11-26 23:05:48 +0900

    studyGenerator

--- main.js
+++ main.js
@@ -797,16 +797,16 @@ class Car extends Vehicle {
 //     console.log("perform cleanup here");
 // }
 
-const book = [
-    "Twinkle, twinkle, little bat!",
-    "How I wonder what you're at!",
-    "Up above the world you fly",
-    "Like a tea tray in the sky.",
-    "Twinkle, twinkle, little bat!",
-    "How I wonder what you're at!",
-];
+// const book = [
+//     "Twinkle, twinkle, little bat!",
+//     "How I wonder what you're at!",
+//     "Up above the world you fly",
+//     "Like a tea tray in the sky.",
+//     "Twinkle, twinkle, little bat!",
+//     "How I wonder what you're at!",
+// ];
 
-const it = book.values();
+// const it = book.values();
 
 // console.log(it.next());
 // console.log(it.next());
@@ -818,23 +818,120 @@ const it = book.values();
 // console.log(it.next());
 // console.log(it.next());
 
-let current = it.next();
-while(!current.done) {
-    console.log(current.value);
-    current = it.next();
-}
+// let current = it.next();
+// while(!current.done) {
+//     console.log(current.value);
+//     current = it.next();
+// }
 
-const it1 = book.values();
-const it2 = book.values();
+// const it1 = book.values();
+// const it2 = book.values();
 
-it1.next();
-it1.next();
+// it1.next();
+// it1.next();
 
-class Log {
-    constructor() {
-        this.messages = [];
-    }
-    add(message) {
-        this.messages.push({message, timestamp: Date.now()});
-    }
+// class Log {
+//     constructor() {
+//         this.messages = [];
+//     }
+//     add(message) {
+//         this.messages.push({message, timestamp: Date.now()});
+//     }
+//     [Symbol.iterator]() {
+//         //return this.messages.values();
+//         let i = 0;
+//         const message = this.message;
+//         return {
+//             next() {
+//                 if(i >= messages.length)
+//                     return { value: undefined, done: true };
+//                 return { value: messages[i++], done: false };
+//             }
+//         }
+//     }
+// }
+
+// const log = new Log();
+// log.add("first day at sea");
+// log.add("spotted whale");
+// log.add("spotted another vessel");
+
+// for(let entry of log) {
+//     console.log(`${entry.message} @ ${entry.timestamp}`);
+// }
+
+// class FibonacciSequence {
+//     [Symbol.iterator]() {
+//         let a = 0, b = 1;
+//         return {
+//             next() {
+//                 let rval = { value: b, done: false };
+//                 b += a;
+//                 a = rval.value;
+//                 return rval;
+//             }
+//         }
+//     }
+// }
+
+// const fib = new FibonacciSequence();
+// let i = 0;
+// for(let n of fib) {
+//     console.log(n);
+//     if(++i > 9) break;
+// }
+
+// function* rainbow() {
+//     yield 'red';
+//     yield 'orange';
+//     yield 'yellow';
+//     yield 'green';
+//     yield 'blue';
+//     yield 'indigo';
+//     yield 'violet';
+// }
+
+// const it = rainbow();
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+// for(let color of rainbow()) {
+//     console.log(color);
+// }
+
+// function* interrogate() {
+//     const name = yield "What is your name?";
+//     const color = yield "What is your favorite color?";
+//     return `${name}'s favorite color is ${color}.`;
+// }
+
+// const it = interrogate();
+// console.log(it.next('123'));
+// console.log(it.next('Ethan'));
+// console.log(it.next('orange'));
+// console.log(it.next('123'));
+
+// function* abc() {
+//     yield 'a';
+//     yield 'b';
+//     return 'c';
+// }
+
+// const it = abc();
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+function printLeapYearStatus() {
+    const year = new Date().getFullYear();
+    if(year % 4 !== 0) console.log(`${year} is NOT a leap year`);
+    else if(year % 100 != 0) console.log(`${year} IS a leap year`);
+    else if(year % 400 != 0) console.log(`${year} is NOT leap year`);
+    else console.log(`${year} IS a leap year`);
 }
