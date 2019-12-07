48808223f7527634b74c6a60c9a49da6f2145929
jonghyeonNoh <beatz148@gmail.com>
2019-12-07 15:55:31 +0900

    restrat

--- main.js
+++ main.js
@@ -930,8 +930,8 @@ class Car extends Vehicle {
 
 function printLeapYearStatus() {
     const year = new Date().getFullYear();
-    if(year % 4 !== 0) console.log(`${year} is NOT a leap year`);
-    else if(year % 100 != 0) console.log(`${year} IS a leap year`);
-    else if(year % 400 != 0) console.log(`${year} is NOT leap year`);
-    else console.log(`${year} IS a leap year`);
+    if(year % 4 !== 0) return false;
+    else if(year % 100 != 0) return true;
+    else if(year % 400 != 0) return false;
+    else return true;
 }
