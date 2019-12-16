8d16934474a059e85a5c1e37a1dc1dab852c5805
jhnoh <beatz4@naver.com>
2019-12-16 22:42:20 +0900

    endChapter14

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 //import { doesNotReject } from "assert";
 
 //import { throws } from "assert";
@@ -982,19 +983,206 @@
 //     console.log(`${fname} contents: ${data}`);
 // });
 
-const fs = require('fs');
 
-fs.readFile('a.txt', function(err, dataA) {
-    if(err) console.error(err);
-    fs.readFile('b.txt', function(err, dataB) {
-        if(err) console.error(err);
-        fs.readFile('c.txt', function(err, dataC) {
-            if(err) console.error(err);
-            setTimeout(function() {
-                fs.writeFile('d.txt', dataA+dataB+dataC, function(err) {
-                    if(err) console.error(err);
-                });
-            }, 60*1000);
+
+// fs.readFile('a.txt', function(err, dataA) {
+//     if(err) console.error(err);
+//     fs.readFile('b.txt', function(err, dataB) {
+//         if(err) console.error(err);
+//         fs.readFile('c.txt', function(err, dataC) {
+//             if(err) console.error(err);
+//             setTimeout(function() {
+//                 fs.writeFile('d.txt', dataA+dataB+dataC, function(err) {
+//                     if(err) console.error(err);
+//                 });
+//             }, 60*1000);
+//         });
+//     });
+// });
+
+// function countdown(seconds) {
+//     return new Promise(function(resolve, reject) {
+//         for(let i=seconds; i>=0; i--) {
+//             setTimeout(function() {
+//                 if(i===13) return reject(new Error("Oh my god"));
+//                 if(i>0) console.log(i + '...');
+//                 else resolve(console.log("GO!"));
+//             }, (seconds-i)*1000);
+//         }
+//     });
+// }
+
+// // countdown(3);
+// countdown(13).then(
+//     function() {
+//         console.log("countdown completed successfully");
+//     },
+//     function(err) {
+//         console.log("countdown experienced an error: " + err.message);
+//     }
+// )
+
+// const EventEmitter = require('events').EventEmitter;
+
+// class Countdown extends EventEmitter {
+//     constructor(seconds, superstitious) {
+//         super();
+//         this.seconds = seconds;
+//         this.superstitious = !!superstitious;
+//     }
+//     go() {
+//         const countdown = this;
+//         const timeoutIds = [];
+//         return new Promise(function(resolve, reject) {
+//             for(let i=countdown.seconds; i>=0; i--) {
+//                 timeoutIds.push(setTimeout(function() {
+//                     if(countdown.superstitious && i===13) {
+//                         //  대기중인 타임아웃 모두 삭제
+//                         timeoutIds.forEach(clearTimeout);
+//                         return reject(new Error("Oh my god"));
+//                     }
+//                     countdown.emit('tick', i);
+//                     if(i===0) resolve();
+//                 }, (countdown.seconds-i)*1000));
+//             }
+//         });
+//     }
+// }
+
+// const c = new Countdown(5);
+
+// c.on('tick', function(i) {
+//     if(i>0) console.log(i + '...');
+// });
+// c.go()
+//     .then(function() {
+//         console.log('GO!');
+//     })
+//     .catch(function(err) {
+//         console.error(err.message);
+//     })
+
+// const c = new Countdown(15, true)
+//     .on('tick', function(i) {
+//         if(i>0) console.log(i + '...');
+//     });
+
+// c.go()
+//     .then(function() {
+//         console.log('GO!');
+//     })
+//     .catch(function(err) {
+//         console.error(err.message);
+//     })
+
+// function launch() {
+//     return new Promise(function(resolve, reject) {
+//         console.log('Lift off!');
+//         setTimeout(function() {
+//             resolve("In orbit!");
+//         }, 2*1000);
+//     });
+// }
+
+// const c = new Countdown(5, true)
+//     .on('tick', i => console.log(i + '...'));
+
+// c.go()
+//     .then(launch)
+//     .then(function(msg) {
+//         console.log(msg);
+//     })
+//     .catch(function(err) {
+//         console.error("Houston, we have a problem...");
+//     })
+
+// function launch() {
+//     return new Promise(function(resolve, reject) {
+//         if(Math.random() < 0.5) return;     // 문제 발생
+//         console.log("Lift off!");
+//         setTimeout(function() {
+//             resolve("In orbit!");
+//         }, 2*1000);
+//     });
+// }
+
+// function addTimeout(fn, timeout) {
+//     if(timeout === undefined) timeout = 1000;
+//     return function(...args) {
+//         return new Promise(function(resolve, reject) {
+//             const tid = setTimeout(reject, timeout, 
+//                 new Error("promise timed out"));
+//             fn(...arge)
+//                 .then(function(...args) {
+//                     clearTimeout(tid);
+//                     resolve(...args);
+//                 })
+//                 .catch(function(...args) {
+//                     clearTimeout(tid);
+//                     reject(...reject);
+//                 });
+//         });
+//     }
+// }
+
+// c.go()
+//     .then(addTimeout(launch, 11*1000))
+//     .then(function(msg) {
+//         console.log(msg);
+//     })
+//     .catch(function(err) {
+//         console.log("Houston, we have a problem: " + err.message);
+//     })
+
+function nfcall(f, ...args) {
+    return new Promise(function(resolve, reject) {
+        f.call(null, ...args, function(err, ...args) {
+            if(err) return reject(err);
+            resolve(args.length<2 ? args[0] : args);
         });
     });
-});
+}
+
+function ptimeout(delay) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(resolve, reject);
+    });
+}
+
+function grun(g) {
+    const it = g();
+    (function iterate(val) {
+        const x = it.next(val);
+        if(!x.done) {
+            if(x.value instanceof Promise) {
+                x.value.then(iterate).catch(err => it.throw(err));
+            } else {
+                setTimeout(iterate, 0, x.value);
+            }
+        }
+    })();
+}
+
+function* theFutureIsNow() {
+    let data;
+    try {
+        data = yield Promise.all([
+            nfcall(fs.readFile, 'a.txt'),
+            nfcall(fs.readFile, 'b.txt'),
+            nfcall(fs.readFile, 'c.txt'),
+        ]);
+    } catch(err) {
+        console.error("Unable to read one or more input files: " + err.message);
+        throw err;
+    }
+
+    yield ptimeout(60*1000);
+    try {
+        yield nfcall(fs.writeFile, 'd.txt', data[0]+data[1]+data[2]);
+    } catch(err) {
+        console.error("Unable to write outout file: " + err.message);
+        throw err;
+    }
+}
+
+grun(theFutureIsNow);
